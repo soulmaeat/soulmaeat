1985cabfbdc5296daa9496a602130558fe0897e4
HaeIn Lee <52684457+seasign10@users.noreply.github.com>
2024-07-05 11:53:13 +0900

    Revert "fix Location.tsx/지도 마커표시"

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Detail } from './pages/Detail';
-import { Location } from './pages/Location';
 import { Layout } from './components/Layout';
 
 const routes = [
   {
     element: <Layout />,
-    children: [
-      { path: '/detail', element: <Detail /> },
-      { path: '/location', element: <Location /> }
-    ],
+    children: [{ path: '/detail', element: <Detail /> }],
   },
 ];
 
