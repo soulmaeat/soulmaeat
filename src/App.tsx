9a152f52030256089819031b5dde9d9545666cb7
eunji <lejgizz@gmail.com>
2024-07-05 10:58:30 +0900

    Style Feat Fix Add

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Detail } from './pages/Detail';
 import { Layout } from './components/Layout';
+import Profile from './pages/Profile';
 
 const routes = [
   {
     element: <Layout />,
-    children: [{ path: '/detail', element: <Detail /> }],
+    children: [
+      { path: '/detail', element: <Detail /> },
+      { path: '/profile', element: <Profile /> },
+    ]
   },
 ];
 
